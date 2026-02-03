e3f2d779199bb14f6d40d20c2f7b3b120a45bebb
Manon09825 <manon_moulin@orange.fr>
2026-02-03 11:39:39 +0100

    modif cli.py

--- cli.py
+++ cli.py
@@ -1,11 +1,19 @@
 import json
+import pathlib
 
-import sys
+import click
 
 import wpextract
 
+@click.command(help = "Extract links from a wikicode file.")
+@click.argument("page_path", type=click.Path(exists=True, path_type=pathlib.Path, readable=True))
+
+def main(page_path: pathlib.Path):
+    print(f"Opening {page_path} with extension {page_path.suffix}")
+    content = page_path.read_text()
+    links = wpextract.extract_links(content)
+    print(json.dumps(links, ensure_ascii=False, indent=2))
+
+
 if __name__ == "__main__":
-    with open(sys.argv[1]) as in_stream:
-        content = in_stream.read()
-        links = wpextract.extract_links(content)
-        print(json.dumps(links, ensure_ascii=False, indent=2))
+    main()
