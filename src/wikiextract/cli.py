5f6f7f64654d838e278f65e7dd3f66af30ec5974
Manon09825 <manon_moulin@orange.fr>
2026-02-03 12:13:29 +0100

    pyproject + sources

--- src/wikiextract/cli.py
+++ src/wikiextract/cli.py
@@ -3,7 +3,7 @@ import pathlib
 
 import click
 
-import wpextract
+import wikiextract
 
 @click.command(help = "Extract links from a wikicode file.")
 @click.argument("page_path", type=click.Path(exists=True, path_type=pathlib.Path, readable=True))
@@ -11,7 +11,7 @@ import wpextract
 def main(page_path: pathlib.Path):
     print(f"Opening {page_path} with extension {page_path.suffix}")
     content = page_path.read_text()
-    links = wpextract.extract_links(content)
+    links = wikiextract.extract_links(content)
     print(json.dumps(links, ensure_ascii=False, indent=2))
 
 
